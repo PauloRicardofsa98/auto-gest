948fe80b6eb5a21a9bd96402e280700227334e9f
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2025-01-10 10:35:16 -0300

    feat: enhance seeding script to include schedule generation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, ScheduleStatus } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
@@ -87,15 +86,28 @@ const generateProducts = async () => {
 };
 const generateServices = async () => {
   // Quantidade de clientes para gerar
-  const generate = 25;
+  const generate = [
+    "Lavagem simples",
+    "Lavagem completa",
+    "Polimento",
+    "Higienização",
+    "Lavagem detalhada",
+    "Lavagem completa com cera",
+    "Lavagem completa com polimento",
+    "Lavagem completa com cera e polimento",
+    "Higienização completa",
+    "Higienização com polimento",
+    "Higienização com cera",
+    "Higienização com cera e polimento",
+    "Higienização com cera e polimento completo",
+    "Higienização com cera e polimento completo e cristalização",
+    "Higienização com cera e polimento completo e cristalização e vitrificação",
+  ];
 
-  const dados: Prisma.ServiceCreateInput[] = Array.from(
-    { length: generate },
-    () => ({
-      name: faker.commerce.productName(),
-      price: faker.commerce.price(),
-    }),
-  );
+  const dados: Prisma.ServiceCreateInput[] = generate.map((service) => ({
+    name: service,
+    price: faker.commerce.price(),
+  }));
 
   await prisma.service.createMany({
     data: dados,
@@ -106,22 +118,28 @@ const generateVehicles = async () => {
   // Quantidade de clientes para gerar
   const generate = 25;
 
-  const dados: Prisma.VehicleCreateInput[] = Array.from(
+  const clients = await prisma.client.findMany();
+
+  const dados: Prisma.VehicleCreateManyInput[] = Array.from(
     { length: generate },
-    () => ({
-      name: faker.vehicle.vehicle(),
-      brand: "FIAT",
-      model: faker.vehicle.model(),
-      year: 2024,
-      color: faker.vehicle.color(),
-      plate: faker.vehicle.vin(),
-    }),
+    () => {
+      const random = Math.floor(Math.random() * clients.length);
+      return {
+        brand: "FIAT",
+        model: faker.vehicle.model(),
+        year: 2024,
+        color: faker.vehicle.color(),
+        plate: faker.vehicle.vin(),
+        clientUuid: clients[random].uuid,
+      };
+    },
   );
 
   await prisma.vehicle.createMany({
     data: dados,
   });
 };
+
 const generateEmployers = async () => {
   // Quantidade de clientes para gerar
   const generate = 25;
@@ -142,16 +160,53 @@ const generateEmployers = async () => {
   });
 };
 
+const generateSchedules = async () => {
+  const generate = 80;
+  const services = await prisma.service.findMany();
+  const vehicles = await prisma.vehicle.findMany({
+    include: {
+      client: true,
+    },
+  });
+  const employers = await prisma.employer.findMany();
+  const status = ["PENDING", "CANCELED", "DONE"];
+
+  const dados: Prisma.ScheduleCreateManyInput[] = Array.from(
+    { length: generate },
+    () => {
+      const randomService = Math.floor(Math.random() * services.length);
+      const randomVehicle = Math.floor(Math.random() * vehicles.length);
+      const randomEmployer = Math.floor(Math.random() * employers.length);
+      const randomStatus = Math.floor(Math.random() * status.length);
+      const statusSelected = status[randomStatus] as ScheduleStatus;
+
+      return {
+        clientUuid: vehicles[randomVehicle].client?.uuid || "",
+        serviceUuid: services[randomService].uuid,
+        employerUuid: employers[randomEmployer].uuid,
+        vehicleUuid: vehicles[randomVehicle].uuid,
+        status: statusSelected,
+        dataSaid: statusSelected === "DONE" ? faker.date.recent() : null,
+        createdAt: faker.date.recent(),
+      };
+    },
+  );
+
+  await prisma.schedule.createMany({
+    data: dados,
+  });
+};
+
 async function main() {
   console.log("Start seeding...");
-  // generateClients();
-  // generateCategoryProducts();
-  // generateSuppliers();
-  // generateProducts();
-  // generateServices();
-  // generateVehicles();
-  generateEmployers();
-
+  await generateClients();
+  await generateCategoryProducts();
+  await generateProducts();
+  await generateSuppliers();
+  await generateServices();
+  await generateVehicles();
+  await generateEmployers();
+  await generateSchedules();
   console.log("Seeding finished.");
 }
 
